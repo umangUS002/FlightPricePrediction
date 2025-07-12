80a915dd6192ad3d02a7502fe73a75a9e52474c6
umangUS002 <btech10271.23@bitmesra.ac.in>
2025-07-12 14:35:33 +0530

    fixed app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ function FlightPricePredictor() {
     stops: '',
     arrival_time: '',
     destination_city: '',
-    class: '',
+    flying_class: '',
     departure_date: '',
   });
 
@@ -168,8 +168,8 @@ function FlightPricePredictor() {
         <div className="space-y-2 w-full">
           <label className="block text-lg font-medium text-yellow-500">Class</label>
           <select
-            name="class"
-            value={formData.class}
+            name="flying_class"
+            value={formData.flying_class}
             onChange={handleChange}
             className="block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
           >
